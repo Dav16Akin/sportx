2f24c51bb4e89453ddc112676d70bcee78c595e5
unknown <akindav16@gmail.com>
2024-12-09 23:59:27 +0100

    fixing deployment issues7

--- app/(root)/checkout/page.tsx
+++ app/(root)/checkout/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import CustomerInfoForm from "@/components/shared/CustomerInfoForm";
 import CustomForm from "@/components/shared/CustomForm";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
